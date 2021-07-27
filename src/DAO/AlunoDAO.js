c00bad75f23f7ca25076b82f1a569c6ea49d954e
Gustavo Souza <souzagh77@gmail.com>
2021-07-27 19:45:09 -0300

    Adicionando método GET/ID e POST

--- src/DAO/AlunoDAO.js
+++ src/DAO/AlunoDAO.js
@@ -16,4 +16,28 @@ module.exports = class AlunoDAO
             })
         })
     }
+
+    getAluno(id)
+    {
+        return new Promise((resolve,reject) => 
+        {
+            this.db.get("SELECT * FROM ALUNOS WHERE id=?",id,(error,rows)=>
+            {
+                if(error) reject(error);
+                else resolve(rows);
+            })
+        })
+    }
+
+    postAluno(newAluno)
+    {
+        return new Promise ((resolve,reject) =>
+        {
+            this.db.get("INSERT INTO ALUNOS (nome,cpf,endereco,bairro,telefone,email,idade,plano) VALUES (?,?,?,?,?,?,?,?) ",Object.values(newAluno),(error,rows) => 
+            {
+                if(error) reject(error)
+                else resolve(rows)
+            })
+        })    
+    }
 }
--- src/controllers/aluno-controller.js
+++ src/controllers/aluno-controller.js
@@ -5,6 +5,26 @@ module.exports = (app,db) =>
 {
     let alunoDB = new AlunoDAO(db);
 
+    app.post('/aluno', async (req,res) =>
+    {
+        const {nome,cpf,endereco,bairro,telefone,email,idade,plano} = req.body
+        let newAluno = new Aluno(nome,cpf,endereco,bairro,telefone,email,idade,plano)
+
+        try {
+            await alunoDB.postAluno(newAluno)
+            res.status(201).json({
+                message:"Aluno inserido com sucesso",
+                error:false
+            })
+        } catch (error)
+        {
+            res.status(500).json({
+                message:"Erro ao inserir aluno",
+                log:error.message,
+                error})    
+        }
+    })
+
     app.get('/aluno', async (req,res) =>
     {
         try{
@@ -16,7 +36,28 @@ module.exports = (app,db) =>
         }
         catch(error)
         {
-            res.status(500).res.json({message:error.message});
+            res.status(500).res.json({error:error.message});
         }
     });
+
+    app.get('/aluno/:id', async (req,res) =>
+    {
+        const { id } = req.params;
+        try {
+            if(parseInt(id))
+            {
+                let result = await alunoDB.getAluno(id)
+                if(result) 
+                    res.json(result)
+                else
+                    throw new Error("Nenhum usuario encontrado");
+            }
+            else
+                throw new Error("É necessário um ID tipo Inteiro, nenhumo outro tipo aceito");
+
+        } catch (error) 
+        {
+            res.status(500).json({error:error.message})    
+        }
+    })
 };
