0788d2d44366be541b7e6f3bc7c9eb5b5e1f59e1
Gustavo Souza <souzagh77@gmail.com>
2021-07-27 19:46:57 -0300

    Teste de Post

--- src/test/AlunoDAO.test.js
+++ src/test/AlunoDAO.test.js
@@ -1,5 +1,6 @@
 const db = require('../infra/database');
 const AlunoDAO = require('../DAO/AlunoDAO');
+const Aluno = require('../models/Aluno-Model')
 
 describe("Testando DAO", () =>
 {
@@ -8,5 +9,12 @@ describe("Testando DAO", () =>
         const alunoDB = new AlunoDAO(db);
         let result = await alunoDB.getAllAlunos();
         expect(Array.isArray(result)).toBeTruthy
+    })
+
+    it('POST', async () =>
+    {
+        const alunoDB = new AlunoDAO(db);
+        let result = await alunoDB.postAluno(new Aluno("Galvão Bueno",25252525252,"Avenida Brasil","Penha",21333333333,"galvaobueno@globo.com",60,2));
+        expect(result).toBe
     })
 })
