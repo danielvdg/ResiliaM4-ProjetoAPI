2e977297cda719e31d243942d3fa9d4a288f4136
Gustavo Souza <souzagh77@gmail.com>
2021-07-27 19:47:44 -0300

    colunas 'cpf' e 'bairro' adicionadas

--- src/infra/criar-e-popular.js
+++ src/infra/criar-e-popular.js
@@ -8,7 +8,9 @@ const ALUNOS_SCHEMA = `
     CREATE TABLE IF NOT EXISTS "Alunos" (
         "id" INTEGER PRIMARY KEY AUTOINCREMENT,
         "nome"     varchar(50),
+        "cpf"      INT(11),
         "endereco" varchar(50),
+        "bairro"   varchar(25),
         "telefone" varchar(11),
         "email"    varchar(30),
         "idade"    INT(3),
@@ -16,10 +18,10 @@ const ALUNOS_SCHEMA = `
     );`;
 
 const ADD_ALUNOS_DATA = `
-    INSERT INTO Alunos(id,nome,endereco,telefone,email,idade,plano)
+    INSERT INTO Alunos(id,nome,cpf,endereco,bairro,telefone,email,idade,plano)
     VALUES
-    (0, 'German Cano', 'Rua Batatinha', '21999999999', 'germancano@vasco.com', 33, 1),
-    (1,'Marta Vieira da Silva','Rua Maria João','21988888888','martavascaina@vasco.com',35,3)
+    (1, 'German Cano',12345678912, 'Rua Batatinha','Barra da Tijuca', '21999999999', 'germancano@vasco.com', 33, 1),
+    (2,'Marta Vieira da Silva',98765432198,'Rua Maria João','Leblon','21988888888','martavascaina@vasco.com',35,3)
     `;
 
 function criaTabelaAlunos()
