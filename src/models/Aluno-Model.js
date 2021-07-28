2d4a23774955ab1bcc78fc64d2e794119aed38f4
Gustavo Souza <souzagh77@gmail.com>
2021-07-28 19:55:28 -0300

    Inicio de validação dos dados

--- src/models/Aluno-Model.js
+++ src/models/Aluno-Model.js
@@ -2,13 +2,28 @@ module.exports = class Aluno
 {
     constructor(nome,cpf,endereco,bairro,telefone,email,idade,plano)
     {
-        this.nome = nome;
-        this.cpf = cpf;
+        if(nome.length > 5)
+            this.nome = nome
+        else throw new Error('Nome muito curto')
+        
+        if(cpf.toString().length == 11)
+            this.cpf = cpf
+        else throw new Error('CPF inválido. O CPF precisa conter 11 dígitos, sem pontuação')
+
         this.endereco = endereco;
         this.bairro = bairro;
-        this.telefone = telefone;
+
+        if(telefone.toString().length == 11)
+            this.telefone = telefone
+        else throw new Error('Telefone inválido. O telefone precisa conter 11 dígitos, sem pontuação')
+
         this.email = email;
-        this.idade = idade;
+
+        if(typeof idade == 'number' && idade > 0)
+            this.idade = idade
+        else throw new Error('Idade inválida')
+
+
         this.plano = plano;
     }
 }
