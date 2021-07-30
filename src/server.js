729ac7bdb1d56397696b5da8334212c281243dd4
Gustavo Souza <souzagh77@gmail.com>
2021-07-29 21:51:42 -0300

    process.env adicionado para deploy no heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const port = 40001;
 
-app.listen(port, () =>
+app.listen(process.env.PORT || port, () =>
 {
-    console.log("Server rodando na porta: " + port)
+    console.log(`Server rodando na porta: ${process.env.PORT || port}`)
 })
